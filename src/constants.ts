fc6585618d6c5d605d1a70decddc73b352ee1594
Michaelwucoc <110759450+Michaelwucoc@users.noreply.github.com>
2025-11-28 00:30:36 +0800

    revert mai mile and fix ticket logout and others

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_MACHINE_INFO = {
+  clientId: 'A63E01E1423',
+  regionId: 28,
+  placeId: 2044,
+  placeName: 'M 娱乐空间南宁兴宁店',
+  regionName: '广西',
+}
+
+export const DEFAULT_TURNSTILE_TOKEN = '1145141919810MyFirstPhone'
+
