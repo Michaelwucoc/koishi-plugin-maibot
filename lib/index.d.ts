64f7210f40076c7cb40cf5df0c106d68123b6659
Michaelwucoc <110759450+Michaelwucoc@users.noreply.github.com>
2025-12-09 14:04:09 +0800

    Update version to 1.0.11, add required machineInfo and turnstileToken fields in configuration, and update default machine info values.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,17 @@
 import { Context, Schema } from 'koishi';
 export declare const name = "maibot";
+export interface MachineInfo {
+    clientId: string;
+    regionId: number;
+    placeId: number;
+    placeName: string;
+    regionName: string;
+}
 export interface Config {
     apiBaseURL: string;
     apiTimeout?: number;
+    machineInfo: MachineInfo;
+    turnstileToken: string;
 }
 export declare const Config: Schema<Config>;
 export declare function apply(ctx: Context, config: Config): void;
